0ad4d00275779c7a2cb5ae445b9fe864da0dd3f8
Moein Masoudi Nejad <perspolis1380@outlook.com>
2020-09-23 07:24:47 +0330

    Update NearestNeighbors.py

--- Nearest Neighbors/NearestNeighbors.py
+++ Nearest Neighbors/NearestNeighbors.py
@@ -3,28 +3,36 @@ import math
 
 
 class NearestNeighbors:
-    def __init__(self, data, label):
+    def __init__(self, data, label, k=1):
         self.x = data
         self.y = label
+        self.k = k
 
     @staticmethod
     def euclid(dataSetSample, NewSample):
-        return math.sqrt((dataSetSample[0] - NewSample[0]) ** 2 + (dataSetSample[1] - NewSample[1]) ** 2)
+        return np.sqrt(np.sum((np.array(dataSetSample) - np.array(NewSample)) ** 2))
 
     @staticmethod
     def manhattan(dataSetSample, NewSample):
-        return math.sqrt(abs(dataSetSample[0] - NewSample[0]) + abs(dataSetSample[1] - NewSample[1]))
+        return np.sqrt(np.sum(abs(np.array(dataSetSample) - np.array(NewSample))))
 
     def predict(self, newX):
         result = []
         for x, y in zip(self.x, self.y):
             cal = self.manhattan(x, newX)
-            result.append([cal, x, y])
-        return sorted(result)
+            result.append([cal, y])
+        result = sorted(result)[:self.k]
+        countGroup = [0, 0]
+        for item in result[:self.k]:
+            if item[1] == 0:
+                countGroup[0] += 1
+            else:
+                countGroup[1] += 1
+        return countGroup.index(max(countGroup))
 
 
 if __name__ == '__main__':
     DataSetData = [[1, 1], [1, 3], [2, 1], [2, 5], [3, 1], [4, 3], [4, 5], [5, 1], [5, 3], [6, 3], [6, 5]]
     DataSetLabel = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
-    N = NearestNeighbors(DataSetData, DataSetLabel)
+    N = NearestNeighbors(DataSetData, DataSetLabel, k=1)
     print(N.predict([3, 3]))
